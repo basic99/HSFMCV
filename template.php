3468dfb50f991ca03b7a745ff20c844c54d3f41b
Jim White <jbw2003@earthlink.net>
2016-02-22 18:07:29 -0500

    add template

---
+++ template.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+
+<!-- update title and meta tags for each new page here -->
+<title>Huguenot Society FMCV - Home</title>
+<meta name="description" content="Home page of the Huguenot Society of the Founders of Manakin in the Colony of Virginia" />
+
+<?php
+include_once "../site/head.php";
+?>
+<script type="text/javascript">
+
+
+</script>
+<style type="text/css">
+
+h4 {
+    font-size: 17px;
+    font-weight: 700;
+}
+
+</style>
+</head>
+<body>
+<div id="page">
+<?php
+include_once "../site/header.php";
+include_once "../site/nav.php";
+?>
+
+<div id="cntnt">
+<input type="hidden" name="modtime" value="<?php echo date('j F Y', getlastmod()); ?>" />
+
+<section>
+<!-- start add content here -->
+
+
+<!-- end of added content here -->
+</section>
+<div id="cse" ></div>
+</div>
+<?php
+include_once "../site/footer.php";
+?>
+</div>
+
+</body>
+</html>
